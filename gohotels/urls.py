902873b069bd25c6dd5b0fdee641815c056c55d6
Matt Mansour <slackbabbath@gmail.com>
2012-07-30 14:41:45 -0700

    Adding new app

--- gohotels/urls.py
+++ gohotels/urls.py
@@ -1,2 +1,9 @@
-__author__ = 'mattmansour'
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from mezzanine.core.views import direct_to_template
+admin.autodiscover()
+
+urlpatterns = patterns('gohotels.views',
+    url("^$", "home", name="home"),
+)
   
--- gohotels/views.py
+++ gohotels/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from gohotels.models import *
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.template import RequestContext
+from django.core import serializers
+import logging
+from mezzanine.utils.views import paginate
+from mezzanine.blog.models import BlogPost, BlogCategory
+
+
+def home(request):
+    blog_post_list = BlogPost.objects.filter(status=2)[:7]
+    for b in blog_post_list:
+        print b
+##        logger.debug('View: home - Success')
+    return render_to_response('index.html',
+                            {'blog_post_list':blog_post_list},
+                             context_instance=RequestContext(request))
