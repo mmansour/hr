1e76ffbc4277ce0e64a3c507c73bec9d4a9eca90
Matt Mansour <slackbabbath@gmail.com>
2012-08-17 11:22:51 -0700

    adding thumbs to hotelpage model

--- gohotels/management/commands/get_images.py
+++ gohotels/management/commands/get_images.py
@@ -7,8 +7,8 @@ class Command(BaseCommand):
     help = 'Pulls hotel images'
     def handle(self, *args, **options):
         filteredimageslist = []
-#        with open('/users/mattmansour/django/sites/dev/hotelretreats/docs/HotelImageList.txt', 'rU') as f:
-        with open('/home/mattym/webapps/hotelretreats/docs/HotelImageList.txt', 'rU') as f:
+        with open('/users/mattmansour/django/sites/dev/hotelretreats/docs/HotelImageList.txt', 'rU') as f:
+#        with open('/home/mattym/webapps/hotelretreats/docs/HotelImageList.txt', 'rU') as f:
 
             reader = csv.reader(f, delimiter='|')
             reader.next() # SKIPS HEADER LINE
@@ -16,17 +16,32 @@ class Command(BaseCommand):
             hotel = HotelPage.objects.all()
             hotel_id_list = [i.hotelid for i in hotel]
             csv_id_list = [[int(col[0]), col[1], col[2], col[6]]  for col in reader]
-            l3 = [h for h in csv_id_list if h[0] in hotel_id_list]
 
-            for i in l3:
-                each_hotel = HotelPage.objects.get(hotelid=i[0])
-                img = HotelImage(
-                    hotel=each_hotel,
-                    alt=i[1],
-                    image_url=i[2].replace('media.expedia.com', 'media.hotelretreats.com').replace('images.travelnow.com', 'media.hotelretreats.com'),
-                    thumbnail_url=i[3].replace('media.expedia.com', 'media.hotelretreats.com').replace('images.travelnow.com', 'media.hotelretreats.com'),
-                )
-                img.save()
+############ GET ALL IMAGES POPULATE RELATED IMAGE MODEL
+#            l3 = [h for h in csv_id_list if h[0] in hotel_id_list]
+#            for i in l3:
+#                each_hotel = HotelPage.objects.get(hotelid=i[0])
+#                img = HotelImage(
+#                    hotel=each_hotel,
+#                    alt=i[1],
+#                    image_url=i[2].replace('media.expedia.com', 'media.hotelretreats.com').replace('images.travelnow.com', 'media.hotelretreats.com'),
+#                    thumbnail_url=i[3].replace('media.expedia.com', 'media.hotelretreats.com').replace('images.travelnow.com', 'media.hotelretreats.com'),
+#                )
+#                img.save()
+
+########### ADD A THUMBNAIL TO MAIN MODEL
+            thumb_list = [[h[0], h[3]] for h in csv_id_list if h[0] in hotel_id_list]
+            thumbdict = {}
+            
+            for t in thumb_list:
+                thumbdict[t[0]] = t[1].replace('media.expedia.com', 'media.hotelretreats.com').replace('images.travelnow.com', 'media.hotelretreats.com')
+
+            for k,v in thumbdict.iteritems():
+                hotelthumb = HotelPage.objects.get(hotelid=k)
+                hotelthumb.thumbnail_url = v
+                hotelthumb.save()
+
+            print 'done'
 
 
 
